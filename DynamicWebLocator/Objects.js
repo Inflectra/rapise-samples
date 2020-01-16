9f5bdf9dda291eb1f86b40d998c0f7afa98a0b97
dmarkovtsev <denis@inflectra.com>
2020-01-16 16:44:28 +0300

    Locator parameters

---
+++ DynamicWebLocator/Objects.js
@@ -0,0 +1,185 @@
+var saved_script_objects={
+	"Username": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Username",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Library Information System - React, Babel6 and Webpack",
+		"xpath": "//input[@id='username']",
+		"title": "Library Information System - React, Babel6 and Webpack",
+		"url": "http://libraryinformationsystem.org/react-js#"
+	},
+	"Password": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Password",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Library Information System - React, Babel6 and Webpack",
+		"xpath": "//input[@id='password']",
+		"title": "Library Information System - React, Babel6 and Webpack",
+		"url": "http://libraryinformationsystem.org/react-js#"
+	},
+	"Login": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "Login",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Library Information System - React, Babel6 and Webpack",
+		"xpath": "//button",
+		"title": "Library Information System - React, Babel6 and Webpack",
+		"url": "http://libraryinformationsystem.org/react-js#"
+	},
+	"Books": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Books",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Library Information System - React, Babel6 and Webpack",
+		"xpath": "//li[1]/a",
+		"title": "Library Information System - React, Babel6 and Webpack",
+		"url": "http://libraryinformationsystem.org/react-js#"
+	},
+	"Edit": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "Edit",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Library Information System - React, Babel6 and Webpack",
+		"xpath": "param_vars://tr[./td/text()='{BookName}']/td[7]/div/button[1]",
+		"title": "Library Information System - React, Babel6 and Webpack",
+		"url": "http://libraryinformationsystem.org/react-js/books",
+		"BookName": "Amsterdam"
+	},
+	"Cancel": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "param:object_name",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "Cancel",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Library Information System - React, Babel6 and Webpack",
+		"xpath": "//button[text()='Cancel']",
+		"title": "Library Information System - React, Babel6 and Webpack",
+		"url": "http://libraryinformationsystem.org/react-js/books"
+	},
+	"Logout": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Logout",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Library Information System - React, Babel6 and Webpack",
+		"xpath": "//p/a",
+		"title": "Library Information System - React, Babel6 and Webpack",
+		"url": "http://libraryinformationsystem.org/react-js/books"
+	}
+};
---
+++ DynamicWebLocator/README.md
@@ -0,0 +1,6 @@
+# DynamicWebLocator
+
+The test shows how to parameterize element locator and use it in a test scenario.
+
+See detailed description [here](https://www.inflectra.com/Support/KnowledgeBase/KB506.aspx).
+
