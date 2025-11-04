43164a68c1fd7b1c4b909c2f5b444a8d3a508b9d
Alexey Grinevich <alexey.grinevich@inflectra.com>
2025-11-04 13:22:05 +0300

    Update to support both Windows and Linux versions.

--- CallingNodeJs/User.js
+++ CallingNodeJs/User.js
@@ -6,16 +6,17 @@ function SeSCallNodeJS(/**string*/script_path, input_data)
 	
 	// Restore packages if needed
 	var scriptFolder = g_helper.GetDirectoryName(script_path);
-	if (!File.FolderExists(scriptFolder+'\\node_modules') && File.Exists(scriptFolder+'\\package.json'))
+	if (!File.FolderExists(scriptFolder+'/node_modules') && File.Exists(scriptFolder+'/package.json'))
 	{
-		var npmCmd = g_helper.ResolvePath("InstrumentJS/npm.cmd");
-		Global.DoLaunch(npmCmd + " install", scriptFolder);
+		var npmCmd = g_helper.ResolvePath("InstrumentJS/npm.cmd") || 'npm';  
+		Global.DoLaunch(npmCmd + " install", scriptFolder, false, false, true);
 	}
 	
 	// Return instrumented verison of file, ready for eval
-	var cmd = g_helper.ResolvePath("InstrumentJS/node.exe");
-	var inFile = scriptFolder+'\\input.json';
-	var outFile = scriptFolder+'\\output.json';
+	var isRapiseNpm = !File.Exists("%ENGINE%/InstrumentJS/node.exe");
+	var cmd = isRapiseNpm? "node" : g_helper.ResolvePath("InstrumentJS/node.exe");
+	var inFile = scriptFolder+'/input.json';
+	var outFile = scriptFolder+'/output.json';
 	
 	File.Write(inFile, JSON.stringify(input_data, null, '\t'));
 	if(File.Exists(outFile))
